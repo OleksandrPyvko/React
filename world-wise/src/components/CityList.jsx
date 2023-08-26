2d61d0940ed8e3d59c2f0119e003076cee77e532
Oleksandr <pyvko.a.g@gmail.com>
2023-08-27 02:02:53 +0300

    initial commit

---
+++ world-wise/src/components/CityList.jsx
@@ -0,0 +1,17 @@
+import CityItem from './CityItem';
+import styles from './CityList.module.css';
+import Spinner from './Spinner';
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  return (
+    <div className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </div>
+  );
+}
+
+export default CityList;
