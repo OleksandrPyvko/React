bc14d830240cc7f92fb88282da9f33a23ee011ac
Oleksandr <pyvko.a.g@gmail.com>
2023-08-27 03:53:49 +0300

    finished appSidebar component

---
+++ world-wise/src/components/CountryList.jsx
@@ -0,0 +1,29 @@
+import CountryItem from './CountryItem';
+import Message from './Message';
+import styles from './CountryList.module.css';
+import Spinner from './Spinner';
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((acc, curr) => {
+    if (!acc.map((el) => el.country).includes(curr.country)) {
+      return [...acc, { country: curr.country, emoji: curr.emoji }];
+    } else return acc;
+  }, []);
+
+  return (
+    <div className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} />
+      ))}
+    </div>
+  );
+}
+
+export default CountryList;
